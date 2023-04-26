5614b0974128c3985713641382141025283ae139
Andrew Osheroff <andrewosh@gmail.com>
2023-04-27 01:56:23 +0200

    Add streaming requests/responses + test (needs more tests)

--- test.js
+++ test.js
@@ -132,3 +132,49 @@ test('send does not get responses', async t => {
     rpc1.recv(data)
   }
 })
+
+test('basic bidirectional stream', async t => {
+  t.plan(6)
+  const rpc1 = new RPC(send1)
+  const rpc2 = new RPC(send2)
+
+  const expected = [1, 2, 2, 3]
+
+  rpc1.register(0, {
+    request: c.uint,
+    response: c.uint,
+    onstream: stream => {
+      stream.on('data', data => {
+        stream.write(data)
+        stream.write(data + 1)
+      })
+      stream.once('end', () => {
+        t.pass('remote stream ended')
+        stream.end()
+      })
+    }
+  })
+  const ping = rpc2.register(0, {
+    request: c.uint,
+    response: c.uint
+  })
+
+  const s = ping.createRequestStream()
+  s.write(1)
+  s.write(2)
+  s.end()
+
+  s.on('data', data => {
+    t.is(data, expected.shift())
+  })
+  s.on('end', () => {
+    t.is(expected.length, 0)
+  })
+
+  function send1 (data) {
+    rpc2.recv(data)
+  }
+  function send2 (data) {
+    rpc1.recv(data)
+  }
+})
